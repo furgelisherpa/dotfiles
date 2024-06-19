0f4dee13089d52ed4844c2089d7d24ab3c50d6e3
furgelisherpa <coffeewise@proton.me>
2024-06-19 16:40:31 +0545

    kanagawa themed

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,10 +16,13 @@
 (setq display-line-numbers-type 'relative)
 ;; completition
 (ido-mode 1)
+;; highlights matching parenthesis
+(show-paren-mode 1)
+
 ;; set per-save backup
 (setq backup-directory-alist '(("" . "~/.emacs.d/backup/per-save")))
 
-;; set per-sessin backup
+;; set per-session backup
 (defun force-backup-of-buffer ()
   (when (not buffer-backed-up)
     (let ((backup-directory-alist '(("" . "~/.emacs.d/backup/per-session")))
@@ -30,16 +33,57 @@
 
 (add-hook 'before-save-hook  'force-backup-of-buffer)
 
+;; for wayland use wl-copy/wl-paste
+;; credit: yorickvP on Github
+(setq wl-copy-process nil)
+(defun wl-copy (text)
+  (setq wl-copy-process (make-process :name "wl-copy"
+                                      :buffer nil
+                                      :command '("wl-copy" "-f" "-n")
+                                      :connection-type 'pipe
+                                      :noquery t))
+  (process-send-string wl-copy-process text)
+  (process-send-eof wl-copy-process))
+(defun wl-paste ()
+  (if (and wl-copy-process (process-live-p wl-copy-process))
+      nil ; should return nil if we're the current paste owner
+      (shell-command-to-string "wl-paste -n | tr -d \r")))
+(setq interprogram-cut-function 'wl-copy)
+(setq interprogram-paste-function 'wl-paste)
+
+;; if selection-mode replace with type text
+(delete-selection-mode)
+
+;; Load the theme file
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(enable-theme 'kanagawa)
+
 ;; installing package manager
 (require 'package)
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")
       ("melpa" . "https://melpa.org/packages/")
       ("org" . "https://orgmode.org/elpa/")))
+
 (package-initialize)
 
 ;; install use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-(setq use-package-always-ensure t)
+
+;; pdf-viewer
+(use-package pdf-tools
+  :ensure t
+  :config
+  (pdf-tools-install)
+  (setq-default pdf-view-display-size 'fit-width)
+  (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward)
+  (add-to-list 'auto-mode-alist '("\\.pdf\\'" . pdf-view-mode))
+  (add-hook 'pdf-view-mode-
+	  (lambda ()
+              (pdf-view-midnight-minor-mode)
+              (setq pdf-view-midnight-colors '("#ffffff" . "#000000")))) ;; White text on black background
+  (setq-default pdf-view-display-size 'fit-width)
+  (setq pdf-view-continuous t)
+  (add-hook 'pdf-view-mode-hook (lambda () (display-line-numbers-mode -1))))
