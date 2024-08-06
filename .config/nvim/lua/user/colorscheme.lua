cd411742b304c577d009dbfb910c84c07912d5e5
CoffeeWise <109355733+furgelisherpa@users.noreply.github.com>
2024-08-06 17:20:25 +0545

    Delete .config/nvim/lua/user directory

--- .config/nvim/lua/user/colorscheme.lua
+++
@@ -1,7 +0,0 @@
--- local colorscheme = "kanagawa-dragon"
-local colorscheme = "gruvbox"
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  return
-end
--- .config/nvim/lua/user/vimtex.lua
+++
@@ -1,6 +0,0 @@
-vim.g.vimtex_view_method = 'zathura'
-vim.g.maplocalleader = ','
-vim.o.foldmethod = "expr"
-vim.o.foldexpr="vimtex#fold#level(v:lnum)"
-vim.o.foldtext="vimtex#fold#text()"
-vim.o.foldlevel=2
