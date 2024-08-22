3ac298a8352b4ddb6a98e349a6fb20943c4fd08b
furgelisherpa <furgelisherpa@tuta.io>
2024-08-22 16:03:21 +0545

    donot attach buffer number

--- .config/nvim/lua/core/autocmd.lua
+++ .config/nvim/lua/core/autocmd.lua
@@ -1,10 +1,11 @@
 -- fix weird naming for terminal windows
+vim.api.nvim_create_augroup('Terminal', { clear = true })
 vim.api.nvim_create_autocmd("TermOpen", {
+  group = 'Terminal',
   callback = function()
     if vim.bo.buftype == "terminal" then
       local buf_num = vim.api.nvim_get_current_buf()
-      vim.api.nvim_buf_set_name(buf_num, "terminal:" .. buf_num)
+      vim.api.nvim_buf_set_name(buf_num, "terminal")
     end
   end,
 })
-
