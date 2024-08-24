70bb898cce647e32075f26ceb136e6c4b1a23527
furgelisherpa <furgelisherpa@tuta.io>
2024-08-24 10:43:44 +0545

    set black as default bg color

--- .config/nvim/lua/core/plugins/colorscheme.lua
+++ .config/nvim/lua/core/plugins/colorscheme.lua
@@ -4,3 +4,6 @@ local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   return
 end
+
+-- override background color to be as dark as the terminal
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'Black' })
