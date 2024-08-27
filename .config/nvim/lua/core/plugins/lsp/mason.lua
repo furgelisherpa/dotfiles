d10c28a2528588da73305038be02b7158cfe5605
furgelisherpa <furgelisherpa@tuta.io>
2024-08-27 22:36:01 +0545

    added jdtls support

--- .config/nvim/lua/core/plugins/lsp/mason.lua
+++ .config/nvim/lua/core/plugins/lsp/mason.lua
@@ -2,48 +2,48 @@ local servers = {
   "marksman",
   "texlab",
   "clangd",
-  "lua_ls"
-  -- "gopls",
+  "lua_ls",
+  "jdtls",
 }
 
 local settings = {
-	ui = {
-		border = "none",
-		icons = {
-			package_installed = "◍",
-			package_pending = "◍",
-			package_uninstalled = "◍",
-		},
-	},
-	log_level = vim.log.levels.INFO,
-	max_concurrent_installers = 4,
+  ui = {
+    border = "none",
+    icons = {
+      package_installed = "◍",
+      package_pending = "◍",
+      package_uninstalled = "◍",
+    },
+  },
+  log_level = vim.log.levels.INFO,
+  max_concurrent_installers = 4,
 }
 
 require("mason").setup(settings)
 require("mason-lspconfig").setup({
-	ensure_installed = servers,
-	automatic_installation = true,
+  ensure_installed = servers,
+  automatic_installation = true,
 })
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status_ok then
-	return
+  return
 end
 
 local opts = {}
 
 for _, server in pairs(servers) do
-	opts = {
-		on_attach = require("core.plugins.lsp.handlers").on_attach,
-		capabilities = require("core.plugins.lsp.handlers").capabilities,
-	}
+  opts = {
+    on_attach = require("core.plugins.lsp.handlers").on_attach,
+    capabilities = require("core.plugins.lsp.handlers").capabilities,
+  }
 
-	server = vim.split(server, "@")[1]
+  server = vim.split(server, "@")[1]
 
-	local require_ok, conf_opts = pcall(require, "core.plugins.lsp.settings." .. server)
-	if require_ok then
-		opts = vim.tbl_deep_extend("force", conf_opts, opts)
-	end
+  local require_ok, conf_opts = pcall(require, "core.plugins.lsp.settings." .. server)
+  if require_ok then
+    opts = vim.tbl_deep_extend("force", conf_opts, opts)
+  end
 
-	lspconfig[server].setup(opts)
+  lspconfig[server].setup(opts)
 end
